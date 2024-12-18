0f3d659123981933f8dea8a5e87ac6f00066da8c
eMac <wtvemac@proton.me>
2024-12-17 23:20:22 -0500

    Fix linker error. Screen isn't used.

--- examples/doom/src/am_map.c
+++ examples/doom/src/am_map.c
@@ -504,7 +504,7 @@ void AM_initVariables(void)
     static event_t st_notify = { ev_keyup, AM_MSGENTERED };
 
     automapactive = true;
-    fb = screens[0];
+    //fb = screens[0];
 
     f_oldloc.x = MAXINT;
     amclock = 0;
